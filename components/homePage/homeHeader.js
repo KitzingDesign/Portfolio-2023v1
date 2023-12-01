f3e87d10eed170b7984180b23eb34573c8b17c8a
KitzingDesign <jakob.kitzing@hotmail.com>
2023-12-01 16:10:55 +0000

    Change for height again

--- components/homePage/homeHeader.js
+++ components/homePage/homeHeader.js
@@ -9,79 +9,78 @@ import styled from "@emotion/styled";
 
 const text = "UX/UI.";
 
-const HomeHeader = () => {
-	const Container = styled.div`
-		height: 100vh;
-		width: 80vw;
-		margin: 0 auto;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		@media (orientation: portrait) {
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			height: 100vh;
-			max-height: -webkit-fill-available;
-		}
-		@media (orientation: portrait) and (max-width: 34.5em) {
-			height: 100%;
-			max-height: -webkit-fill-available;
-		}
-	`;
-	const HeaderElement = styled.div`
-		display: flex;
-		height: 100vh;
-		width: 100%;
-		align-items: center;
+const Container = styled.div`
+	height: 100vh;
+	width: 80vw;
+	margin: 0 auto;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	@media (orientation: portrait) {
+		flex-direction: column;
 		justify-content: center;
-		@media (orientation: portrait) {
-			flex-direction: column-reverse;
-		}
-	`;
+		align-items: center;
+		height: 100vh;
+	}
+	@media (orientation: portrait) and (max-width: 34.5em) {
+		height: 100vh;
+		max-height: -webkit-fill-available;
+	}
+`;
+const HeaderElement = styled.div`
+	display: flex;
+	height: 100vh;
+	width: 100%;
+	align-items: center;
+	justify-content: center;
+	@media (orientation: portrait) {
+		flex-direction: column-reverse;
+	}
+`;
 
-	const HeaderText = styled.div`
-		width: 80%;
+const HeaderText = styled.div`
+	width: 80%;
+	& > p {
+		line-height: 1.6;
+		width: 90%;
+		margin-bottom: 4.8rem;
+		margin-top: 3.2rem;
+	}
+	& > h1 {
+		font-size: 5.2rem;
+	}
+	@media (orientation: portrait) {
+		width: 100%;
 		& > p {
-			line-height: 1.6;
-			width: 90%;
-			margin-bottom: 4.8rem;
-			margin-top: 3.2rem;
-		}
-		& > h1 {
-			font-size: 5.2rem;
-		}
-		@media (orientation: portrait) {
 			width: 100%;
-			& > p {
-				width: 100%;
-			}
 		}
-	`;
-	const Btn = styled.a`
-		box-sizing: inline-block;
-		font-size: 2rem;
-		font-weight: bold;
-		background-color: black;
-		border: none;
-		padding: 1.6rem 3.2rem;
-		color: white;
-		text-decoration: none;
-		border-radius: 6px;
+	}
+`;
+const Btn = styled.a`
+	box-sizing: inline-block;
+	font-size: 2rem;
+	font-weight: bold;
+	background-color: black;
+	border: none;
+	padding: 1.6rem 3.2rem;
+	color: white;
+	text-decoration: none;
+	border-radius: 6px;
+	cursor: pointer;
+	&:hover {
+		background: white;
+		padding: 1.4rem 3rem;
+		border: 0.2rem black solid;
+		box-shadow: 2px black;
+		color: black;
 		cursor: pointer;
-		&:hover {
-			background: white;
-			padding: 1.4rem 3rem;
-			border: 0.2rem black solid;
-			box-shadow: 2px black;
-			color: black;
-			cursor: pointer;
-		}
-	`;
-	const HeaderImg = styled.div`
-		width: 80%;
-	`;
+	}
+`;
+const HeaderImg = styled.div`
+	width: 80%;
+`;
 
+const HomeHeader = () => {
 	return (
 		<div>
 			<Container>
