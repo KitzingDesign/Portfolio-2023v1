f98381af4d36edda454e5de3937ebd5c56714e12
Jakob Kitzing <Jakob.kitzing@hotmail.com>
2025-01-08 14:01:55 +0100

    new popup

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import ToastModal from "../UI/toastModal/ToastModal";
 import Nav from "./Navigation";
 
 export default function Layout({ children }) {
-	return (
-		<>
-			<Nav />
-			<main>{children}</main>
-		</>
-	);
+  const [matPopup, setMatPopup] = useState(true);
+
+  const closePopup = () => {
+    setMatPopup(false);
+  };
+  return (
+    <>
+      <Nav />
+      <main>{children}</main>
+
+      {matPopup && (
+        <ToastModal
+          message="I’ve coded an online cook book! Want to check it out?"
+          buttonText="Bring me there!"
+          onButtonClick={() => {}}
+          onClose={closePopup}
+        />
+      )}
+    </>
+  );
 }
